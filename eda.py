cc44dfe6baeea0901acb5fe88a0215a70b332706
GrantPerkins <gcperk20@gmail.com>
2022-12-04 12:44:20 -0500

    Functions

--- eda.py
+++ eda.py
@@ -2,5 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("diabetes.csv")
-df.describe().round(3).to_csv("tables/summary_stats.csv")
+
+def make_summary_table(df):
+    df.describe().round(3).to_csv("tables/summary_stats.csv")
+
+
+if __name__ == "__main__":
+    diabetes_df = pd.read_csv("diabetes.csv")
+    make_summary_table(diabetes_df)
